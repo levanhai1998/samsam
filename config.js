d84ee7b61e3005e27d21984510b6a2c3995b0682
levanhai1998 <86667736+levanhai1998@users.noreply.github.com>
2022-03-08 19:29:43 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '💘sam sam💘';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng sam sam😁';
+const contentCard = 'Chúc sam 8-3 luôn xinh đẹp, vui vẻ, thành công và gặp nhiều may mắn❤️';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'sam.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
